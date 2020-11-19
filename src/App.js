292aa4b76a5bb34b41e0704d6b28416c3a124752
lovecosma <williamsmatthew333@gmail.com>
2020-11-18 19:23:35 -0500

    initializing painter-react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
+import Painter from  './containers/Painter'
+import SampleForm from './containers/SampleForm'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <SampleForm/>
+   </div>
   );
 }
 
---
+++ src/containers/SampleForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SampleForm extends Component {
+    render() {
+        return (
+            <div>
+                Hey
+            </div>
+        )
+    }
+}
+
+export default SampleForm
