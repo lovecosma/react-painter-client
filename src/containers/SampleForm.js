bcc55cd3574a324b7f51e4c7b30d0d278c8fffa7
lovecosma <williamsmatthew333@gmail.com>
2020-11-21 14:59:36 -0500

    Handle input change in form

--- src/containers/SampleForm.js
+++ src/containers/SampleForm.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
+import { theWindow } from 'tone/build/esm/core/context/AudioContext'
 
 export class SampleForm extends Component {
+    state={
+        file: "",
+        name: ""
+    }
+
+    handleFileChange = e => {
+        if(e.target.files[0]){
+            this.setState({
+                [e.target.name]: e.target.files[0]
+            }) 
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+
     render() {
         return (
             <div>
                 <form action="">
                     <label for="file"></label>
-                    <input type="file" name="file" id=""></input><br/>
+                    <input onChange={this.handleFileChange} type="file" name="file" id=""></input><br/>
                     <label for="name"></label>
-                    <input type="text" name="name" id=""></input><br/>
+                    <input onChange={this.handleChange} type="text" name="name" id=""></input><br/>
                     <button type="submit">Submit</button>
                 </form>
             </div>
